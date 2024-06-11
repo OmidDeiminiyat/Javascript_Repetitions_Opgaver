4e260fff4882ee372e931580a2e8e38d4bb85641
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-06-11 10:49:22 +0200

    Lektion 1 Done

--- js/lektion_1.js
+++ js/lektion_1.js
@@ -17,3 +17,94 @@ function fullName(firstName, lastName) {
 
 // Function Calls
 fullName("Ukendt", "Person");
+
+
+function fullName(elementId, test) {
+  if (elementId == 'Omid') {
+    L1_2.classList.toggle('hidden');
+  } else {L1_2.classList.toggle('visible');}
+}
+
+
+// opgave 3
+
+L1_3.addEventListener('click', function() {
+  var h3Element = document.getElementById('opgave3');
+  var messageElement = document.getElementById('message');
+  
+  h3Element.classList.toggle('red');
+  
+  if (h3Element.classList.contains('red')) {
+    messageElement.classList.remove('hidden');
+  } else {
+    messageElement.classList.add('hidden');
+  }
+});
+
+// Opgave 4
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  let count = 0; 
+  if (localStorage.getItem('clickCount')) {
+    count = parseInt(localStorage.getItem('clickCount'), 10);
+  }
+
+  const button = document.getElementById('L1_4'); 
+  const clickCountDisplay = document.getElementById('clickCount'); 
+
+  function updateClickCount() {
+    count += 1; 
+    clickCountDisplay.textContent = `Number of clicks: ${count}`; 
+  }
+  button.addEventListener('click', updateClickCount);
+  window.addEventListener('beforeunload', () => {
+    localStorage.setItem('clickCount', count);
+  });
+});
+
+
+// Opgave5
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const inputField = document.getElementById('wordInput'); 
+  const submitButton = document.getElementById('submitButton'); 
+  const wordDisplay = document.getElementById('wordDisplay'); 
+  const lengthDisplay = document.getElementById('lengthDisplay'); 
+
+
+  function displayWordInfo(word) {
+    const wordLength = word.length; 
+    wordDisplay.textContent = `Word: ${word}`; 
+    lengthDisplay.textContent = `Length: ${wordLength}`; 
+  }
+
+
+  submitButton.addEventListener('click', () => {
+    const word = inputField.value; 
+    displayWordInfo(word); 
+  });
+});
+
+// Opgave 6
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const inputsection = document.getElementById('wordInput'); 
+  const submitField = document.getElementById('submitButton'); 
+  const cleanedWordDisplay = document.getElementById('cleanedWordDisplay'); 
+
+ 
+  function removeVowels(word) {
+    return word.replace(/[aeiouAEIOU]/g, ''); 
+  }
+
+  function displayCleanedWord(word) {
+    const cleanedWord = removeVowels(word); 
+    cleanedWordDisplay.textContent = `Cleaned word: ${cleanedWord}`; 
+  }
+
+
+  submitField.addEventListener('click', () => {
+    const word = inputsection.value; 
+    displayCleanedWord(word); 
+  });
+});
--- js/lektion_2.js
+++ js/lektion_2.js
@@ -3,3 +3,5 @@ let L2_2 = document.querySelector("#L2_2");
 let L2_3 = document.querySelector("#L2_3");
 let L2_4 = document.querySelector("#L2_4");
 let L2_5 = document.querySelector("#L2_5");
+
+
